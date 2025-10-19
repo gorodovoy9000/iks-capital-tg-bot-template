10efa8751407a3ca1c785be49ffcf4d8e1beafcd
roma <romanover9000@gmail.com>
2025-10-19 14:17:03 +0300

    Fixed alembic env.py for schemas.

--- migrations/env.py
+++ migrations/env.py
@@ -48,7 +48,7 @@ def run_migrations_offline() -> None:
 
 
 def do_run_migrations(connection: Connection) -> None:
-    context.configure(connection=connection, target_metadata=target_metadata)
+    context.configure(connection=connection, target_metadata=target_metadata, include_schemas=True)
 
     with context.begin_transaction():
         context.run_migrations()
